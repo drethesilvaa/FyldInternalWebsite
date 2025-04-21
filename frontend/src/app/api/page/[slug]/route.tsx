dfa3e7b7c8aa00083b9a585b2877b8f85865ae01
drethesilva <andressilva03@gmail.com>
2025-04-21 19:01:18 +0100

    slug mismatch

--- frontend/src/app/api/page/[slug]/route.tsx
+++ frontend/src/app/api/page/[slug]/route.tsx
@@ -1,27 +1,27 @@
-// src/app/api/page/[slug]/route.ts
 import { NextRequest, NextResponse } from "next/server";
-import { request, gql } from "graphql-request";
+import { request } from "graphql-request";
 import { GRAPHQL_ENDPOINT, GRAPHQL_HEADERS } from "@/lib/apiConfig";
 import { GET_PAGE_DATA } from "@/graphql/queries/pageData";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { slug: string } }
+  context: { params: { slug: string } }
 ) {
-  const { slug } = await params;
-
-  const pageSlug = slug;
+  const { slug } = context.params;
 
   try {
     const data: any = await request(
       GRAPHQL_ENDPOINT,
       GET_PAGE_DATA,
-      { slug: pageSlug },
+      { slug },
       GRAPHQL_HEADERS
     );
+
     return NextResponse.json(data.pages_connection?.nodes[0]);
   } catch (error) {
-    console.error(error);
-    return NextResponse.error();
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
