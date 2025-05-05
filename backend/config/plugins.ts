fd45f7ac8c55ee9d1afd2ef07ef7ee051d01fc39
drethesilva <andressilva03@gmail.com>
2025-05-05 18:33:33 +0100

    update on strapi plugins

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -6,6 +6,12 @@ export default ({ env }) => ({
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
+        secure: true, 
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {}, 
+        delete: {},
       },
     },
   },
