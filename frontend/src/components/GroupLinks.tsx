e1780a38efb4e16b4db10f389c3ddb99848892da
drethesilva <andressilva03@gmail.com>
2025-05-26 11:26:37 +0100

    motion on group links

--- frontend/src/components/GroupLinks.tsx
+++ frontend/src/components/GroupLinks.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/navigation"
+import { motion } from "framer-motion";
 
 interface GroupLinkProps {
   links: {
@@ -16,6 +17,19 @@ interface GroupLinkProps {
   }[]
 }
 
+const cardVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: (i: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+      delay: i * 0.1,
+    },
+  }),
+};
+
 
 
 export const GroupLinks: React.FC<GroupLinkProps> = ({ links }) => {
@@ -28,8 +42,15 @@ export const GroupLinks: React.FC<GroupLinkProps> = ({ links }) => {
 
 
   return <div className="grid grid-cols-2 gap-4">
-    {links.map((link) => (
-      <div className="card lg:card-side bg-base-100 shadow-sm">
+    {links.map((link, i) => (
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={cardVariants}
+        className="card lg:card-side bg-base-100 shadow-sm"
+        key={i}
+      >
         <figure className="aspect-video">
           <img
             src={link.page.pageBanner.url || "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"}
@@ -42,7 +63,7 @@ export const GroupLinks: React.FC<GroupLinkProps> = ({ links }) => {
             <button onClick={() => { router.push(`${link.page.ParentPage.slug}/${link.page.slug}`) }} className="btn btn-primary">Aceder</button>
           </div>
         </div>
-      </div>
+      </motion.div>
     ))}
   </div>
 };
