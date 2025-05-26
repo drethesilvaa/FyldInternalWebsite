b558df9a228f8982a4b399fc0c961571a3c7d25b
drethesilva <andressilva03@gmail.com>
2025-05-26 15:24:10 +0100

    group links without image

--- frontend/src/components/GroupLinks.tsx
+++ frontend/src/components/GroupLinks.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/navigation"
-import { motion } from "framer-motion";
+import { animate, motion } from "framer-motion";
 
 interface GroupLinkProps {
   links: {
@@ -28,6 +28,7 @@ const cardVariants = {
       delay: i * 0.1,
     },
   }),
+
 };
 
 
@@ -48,19 +49,14 @@ export const GroupLinks: React.FC<GroupLinkProps> = ({ links }) => {
         whileInView="visible"
         viewport={{ once: true, amount: 0.2 }}
         variants={cardVariants}
-        className="card lg:card-side bg-base-100 shadow-sm lg:max-h-52"
+        className="card bg-base-100 card-md shadow-sm lg:max-h-52 hover:-translate-y-1.5 transition-transform"
         key={i}
       >
-        <figure className="aspect-video">
-          <img
-            src={link.page.pageBanner.url || "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"}
-            alt={link.page.pageBanner.alternativeText || "Placeholder Page Picture"} />
-        </figure>
         <div className="card-body">
           <h2 className="card-title">{link.linkTitle}</h2>
           <p>Clica no botão para aceder à pagina {link.linkTitle}</p>
           <div className="card-actions justify-end">
-            <button onClick={() => { router.push(`${link.page.ParentPage.slug}/${link.page.slug}`) }} className="btn btn-primary">Aceder</button>
+            <button onClick={() => { router.push(`${link.page.ParentPage.slug}/${link.page.slug}`) }} className="btn btn-secondary">ACEDER</button>
           </div>
         </div>
       </motion.div>
