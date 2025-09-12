b2e6d93b7e0b9dbc9b623720cf6d32ff50be9a50
drethesilvaa <andressilva03@gmail.com>
2025-09-12 18:20:09 +0100

    plugins update

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,43 +1,17 @@
-module.exports = ({ env }) => {
-  const connectionString = env("AZURE_STORAGE_CONNECTION_STRING");
-  const account = env("AZURE_STORAGE_ACCOUNT") || env("AZURE_ACCOUNT");
-  const accountKey =
-    env("AZURE_STORAGE_ACCOUNT_KEY") || env("AZURE_ACCOUNT_KEY");
-  const sasToken = env("AZURE_STORAGE_SAS_TOKEN") || env("AZURE_SAS_TOKEN");
-  const containerName =
-    env("AZURE_STORAGE_CONTAINER") || env("AZURE_CONTAINER") || "uploads";
-  const cdnBaseURL = env("AZURE_CDN_URL") || env("CDN_URL");
-  const serviceBaseURL = env("AZURE_STORAGE_BASEURL");
-
-  console.log("[upload:azure] resolved options", {
-    hasConnStr: !!connectionString,
-    account,
-    hasAccountKey: !!accountKey,
-    hasSasToken: !!sasToken,
-    containerName,
-  });
-
-  const credentials = connectionString
-    ? { connectionString }
-    : sasToken
-      ? { sasToken }
-      : accountKey
-        ? { accountKey }
-        : {};
-
-  return {
-    upload: {
-      config: {
-        provider: "strapi-provider-upload-azure-storage",
-        providerOptions: {
-          account,
-          containerName,
-          defaultPath: "assets",
-          cdnBaseURL,
-          serviceBaseURL,
-          ...credentials,
-        },
+// Strapi v5 style
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-azure-storage", // or the TS variant from the Market
+      providerOptions: {
+        // If using connection string:
+        connectionString: env("AZURE_STORAGE_CONNECTION_STRING"),
+        // Or, if using account/key (do not set connectionString at the same time):
+        // account: env('AZURE_STORAGE_ACCOUNT'),
+        // accountKey: env('AZURE_STORAGE_ACCOUNT_KEY'),
+        containerName: env("AZURE_STORAGE_CONTAINER", "hr-internal-website"),
+        // optional: basePath, cdnBaseUrl, etc.
       },
     },
-  };
-};
+  },
+});
