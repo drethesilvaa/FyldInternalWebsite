ebe32fe80b9e60171ccce650bae24aba65d39972
drethesilvaa <andressilva03@gmail.com>
2025-09-12 17:33:56 +0100

    plugin update

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,14 +1,14 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: "strapi-provider-upload-azure-storage",
       providerOptions: {
-        account: env("AZURE_STORAGE_ACCOUNT"),
-        accountKey: env("AZURE_STORAGE_ACCOUNT_KEY"), // or SAS token
-        serviceBaseURL: env("AZURE_STORAGE_BASEURL", undefined), // optional custom domain
+        account: env("AZURE_STORAGE_ACCOUNT"), // map to your current var
+        accountKey: env("AZURE_STORAGE_ACCOUNT_KEY"), // or sasToken: env('AZURE_STORAGE_SAS_TOKEN')
         containerName: env("AZURE_STORAGE_CONTAINER", "uploads"),
         defaultPath: "assets",
-        cdnBaseURL: env("AZURE_CDN_URL", undefined),
+        cdnBaseURL: env("AZURE_CDN_URL"),
+        serviceBaseURL: env("AZURE_STORAGE_BASEURL"),
       },
     },
   },
